e59aa3b441d5692f89ad5055bf3b541287d0dcb4
theochen <theochens@gmail.com>
2016-05-26 03:34:35 +0800

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,18 @@
 # config valid only for current version of Capistrano
 lock '3.5.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'pivipedik'
+set :repo_url, 'git@github.com:defendendo/pivipedik.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/home/deploy/pivipedik'
 
 # Default value for :scm is :git
 # set :scm, :git
-
+ 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
 
@@ -28,6 +28,9 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
+
+set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
